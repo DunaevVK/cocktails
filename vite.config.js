cd36e0ee3d684d3931a2b7001abcb0264c87e2f4
Dunaev Vladimir <dunaevvk@gmail.com>
2023-12-10 18:40:24 +0300

    fixed vite.config and images path

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      images: path.resolve(__dirname, 'src/assets/img/'),
     }
   }
 })
